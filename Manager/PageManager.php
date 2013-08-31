b594a3b335f889449fe527dd5744b911d79abd25
gnal <qwertypianox@gmail.com>
2013-08-31 14:07:44 -0400

    fix

--- Manager/PageManager.php
+++ Manager/PageManager.php
@@ -8,14 +8,21 @@ class PageManager extends BaseManager
 {
     public function findByRoute($route)
     {
-        $page = $this->getFindByQueryBuilder(array('t.published' => true, 'a.route' => $route), array('a.translations' => 't', 'a.blocks' => 'b'), array('b.position' => 'ASC'))->getQuery()->getResult();
+        $page = $this->getFindByQueryBuilder(
+            [
+                'translations.published' => true,
+                'a.route' => $route,
+            ],
+            [
+                'a.translations' => 'translations',
+                'a.blocks' => 'blocks',
+                'blocks.translations' => 'blocks_translations',
+            ],
+            [
+                'blocks.position' => 'ASC',
+            ]
+        )->getQuery()->execute();
 
-        if (!isset($page[0])) {
-            $page = null;
-        } else {
-            $page = $page[0];
-        }
-
-        return $page;
+        return isset($page[0]) ? $page[0] : null;
     }
 }
